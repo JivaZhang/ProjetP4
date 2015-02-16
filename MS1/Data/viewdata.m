738a68e81d0f87296b1ddab42414b6b6767e98d9
obronchain <olivier.bronchain@student.uclouvain.be>
2015-02-16 17:28:44 +0100

    compute delay on 1 pulse

--- MS1/Data/viewdata.m
+++ MS1/Data/viewdata.m
@@ -1,5 +1,12 @@
-x = load('P1_cal.tsv');
+x = load('P6_cal.tsv');
+Tx = [336.1 15];
+Rx1 = [160.2 -416];
+Rx2 = [340 -416];
+Rx3 = [66.4 543.4];
 
+n1 = norm(Rx1- Tx)
+n2 = norm(Rx2 - Tx)
+n3 = norm(Rx3 - Tx)
 t = x(:,1);
 n = 1;
 
@@ -7,17 +14,17 @@ while(t(n) < 0.9e-7)
     n = n+1;
 end
 
-t = t(1:n);
-s = x(1:n , 2);
-r = x(1:n, 4);
+for i=3:5 
+    t = t(1:n);
+    s = x(1:n, 2);
+    r = x(1:n, i);
 
-plot(t , r , t , s);
-
-S = fft(s);
-R = fft(r);
-
-C = R .* conj(S);
-
-c = ifft(C);
+    S = fft(s);
+    R = fft(r);
+    C = R .* conj(S);
+    c = ifft(C);
+    delay = t(find(c == max(c(:))))
+end
 
 plot(t , c);
+
