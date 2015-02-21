87e3e52f90f01e21ccad795994fe7c4ccec1b7e4
obronchain <olivier.bronchain@student.uclouvain.be>
2015-02-21 19:22:33 +0100

    Newton findTarget.m

--- MS1/Data/findTarget.m
+++ MS1/Data/findTarget.m
@@ -1,11 +1,35 @@
-function [ target , fval] = findTarget( Z , d )
- Z1 = Z(1,:);
- Z2 = Z(2,:);
- Z3 = Z(3,:);
- 
- err = @(u) (sqrt((u(1) - Z1(1))^2 + (u(2) - Z1(2))^2) - d(1))^2 + (sqrt((u(1) - Z2(1))^2 + (u(2) - Z2(2))^2) - d(2))^2 + (sqrt((u(1) - Z3(1))^2 + (u(2) - Z3(2))^2) - d(3))^2;
- [target,fval] = fminsearch(err, [0 0] , optimset('TolX',1e-8));
-
-
+function [target] = findTarget( Z , r )
+x = 1;
+y = 1;
+delta = 1000;
+n = 0 ;
+while( norm(delta) >= 1e-10 && n<=40)
+    dx = 0;
+    dy = 0;
+    ddxx = 0;
+    ddyy = 0;
+    ddxy = 0;
+    ddyx = 0;
+    
+    for i= 1:3
+        px = Z(i , 1);
+        py = Z(i , 2);
+        d = r(i);
+        dx = dx + ((2*px - 2*x)*(d - ((px - x)^2 + (py - y)^2)^(1/2)))/((px - x)^2 + (py - y)^2)^(1/2);
+        dy = dy + ((2*py - 2*y)*(d - ((px - x)^2 + (py - y)^2)^(1/2)))/((px - x)^2 + (py - y)^2)^(1/2);
+        ddxx = ddxx + (2*px - 2*x)^2/(2*((px - x)^2 + (py - y)^2)) - (2*(d - ((px - x)^2 + (py - y)^2)^(1/2)))/((px - x)^2 + (py - y)^2)^(1/2) + ((2*px - 2*x)^2*(d - ((px - x)^2 + (py - y)^2)^(1/2)))/(2*((px - x)^2 + (py - y)^2)^(3/2));
+        ddxy = ddxy +((2*px - 2*x)*(2*py - 2*y))/(2*((px - x)^2 + (py - y)^2)) + ((2*px - 2*x)*(2*py - 2*y)*(d - ((px - x)^2 + (py - y)^2)^(1/2)))/(2*((px - x)^2 + (py - y)^2)^(3/2));
+        ddyx = ddyx + ((2*px - 2*x)*(2*py - 2*y))/(2*((px - x)^2 + (py - y)^2)) + ((2*px - 2*x)*(2*py - 2*y)*(d - ((px - x)^2 + (py - y)^2)^(1/2)))/(2*((px - x)^2 + (py - y)^2)^(3/2));
+        ddyy = ddyy + (2*py - 2*y)^2/(2*((px - x)^2 + (py - y)^2)) - (2*(d - ((px - x)^2 + (py - y)^2)^(1/2)))/((px - x)^2 + (py - y)^2)^(1/2) + ((2*py - 2*y)^2*(d - ((px - x)^2 + (py - y)^2)^(1/2)))/(2*((px - x)^2 + (py - y)^2)^(3/2))
+    end
+    
+    A = [ ddxx ddyx ; ddxy  ddyy] ;
+    B = [ dx ; dy ];
+    delta = - A \ B;
+    x = delta(1) + x 
+    y = delta(2) + y 
+    n = n +1;
+end
+ target = [ x y];
 end
 
