35b13709cb031f49a1c62181f37d4262bbe4a2d5
obronchain <olivier.bronchain@student.uclouvain.be>
2015-03-01 20:10:09 +0100

    recovering bugs

--- MS2/Data/findA.m
+++ MS2/Data/findA.m
@@ -1,18 +1,18 @@
 function [A] = findA(t , s , r)
 
-n = 0;
-delta = 100000;
-A = 1;
-
-while n<1000 && abs(delta) > 1e-4
-    prod = s - A.*r;
-    val = trapz(t , -2*A*prod);
-    diff = trapz(t , -2*s + 4*A);
-    delta = val/diff;
-    A = A-delta;
-    n = n +1;
-end
-n
+% n = 0;
+% delta = 100000;
+% A = 1200;
+% 
+% while n<1000 && abs(delta) > 1e-4
+%     prod = s - A*r;
+%     val = trapz(t , -2*A*prod);
+%     diff = trapz(t , -2*s + 4*A);
+%     delta = val/diff;
+%     A = A-delta;
+%     n = n +1;
+% end
+% n
 
 %   n = 0;
 %   delta = 100000;
@@ -27,13 +27,12 @@ n
 %   
 %   figure;
 %   plot(Av , Int);
-%   
+%  
 
 
-%fun= @(A) sum((s-A*r).^2);
-%[x,fval] = fminsearch(fun,[1000])
-%A=fval
-  
+fun= @(A) sum((s-A*r).^2);
+[A,fval] = fminsearch(fun,[1000]); 
+
   
 end
 
--- MS2/Data/ms2.m
+++ MS2/Data/ms2.m
@@ -1,15 +1,16 @@
 %x = load('P2_cal.tsv');
 close all;
-l = length(tcal);
-l = l / 200;
+load('multipath_CC');
+l = length(t);
+l = l/100;
 
-t = tcal(1:l);
+t = t(1:l);
 s = gcal(1:l)';
-r = r1cal(1:l)';
+r = -r3bp(1:l);
 fs = 1/(t(2)-t(1));
 S = fft(s);
 f = fs/2 * linspace( 0 , 1 , length(s));
-filter = (f >= 0.7e9) .* ( f<=1.8e9);
+filter = (f >= 0.7e9) .* ( f<=2e9);
 r = ifft(fft(r).*filter);
 
 
@@ -20,11 +21,24 @@ figure;
 plot(t , r , t , sigdelay);
 legend( 'r' , 'sigdelay');
 A = findA( t , s , sigdelay);
-A = 600;
+%A = 1500;
 
+sr = s- A*sigdelay;
 figure;
-plot( t , s , t , s-A*sigdelay , t  , A*sigdelay );
-legend('s ' , 's-A*sigdelay' , 'A*sigdelay' );
+plot( t , s ,  t  , A*sigdelay , t  , s - A*sigdelay );
+legend('s ' ,  'A*sigdelay' , 's-A*sigdelay' );
 
 figure;
 plot(t , s-A*sigdelay);
+
+Av = linspace(-1000, 1000 , 1000);
+in = Av ;
+
+for i = 1:1000
+    in(i) = trapz( t , (s-Av(i)*sigdelay).^2);
+end
+
+plot(Av , in);
+
+
+
